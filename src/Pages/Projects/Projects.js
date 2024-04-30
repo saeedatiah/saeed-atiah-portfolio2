90a015f89237263d6e54569caadd904adbb23ca9
saeedatiah <66546810+saeedatiah@users.noreply.github.com>
2024-04-30 16:20:20 +0300

    add style projects

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -3,24 +3,85 @@ import styles from './Projects.module.css'
 
 const Projects = () => {
   return (
-    <div className=''>
-    <div class="box">
-  <div>
-  <div className={styles.aa}>
-  safdfsdf
+<div>
+<div className={styles.super_box}>
+  <div className={styles.container}>
+  
+    <div className={styles.box}>
+    
+      <div className={styles.icon_bg}>
+      </div>
+    </div>
+
+    <div className={styles.icon}>
+
+      icon
+    </div>
+
   </div>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-      in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. At vero eos et accusamus et iusto odio dignissimos ducimus
-      qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
-      quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem
-      quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur
-      aut perferendis doloribus asperiores repellat.
+
+  <div className={styles.text}>
+    <p clclassNameass={styles.title}>
+      Security Hardening
     </p>
   </div>
 </div>
-      
+
+<div className={styles.super_box}>
+  <div className={styles.container}>
+  
+    <div className={styles.box}>
+    
+      <div className={styles.icon_bg}>
+      </div>
     </div>
+
+    <div className={styles.icon}>
+
+      icon
+    </div>
+
+  </div>
+
+  <div className={styles.text}>
+    <p clclassNameass={styles.title}>
+      Security Hardening
+    </p>
+  </div>
+</div>
+
+<div className={styles.super_box}>
+  <div className={styles.container}>
+  
+    <div className={styles.box}>
+    
+      <div className={styles.icon_bg}>
+      </div>
+    </div>
+
+    <div className={styles.icon}>
+
+      icon
+    </div>
+
+  </div>
+
+  <div className={styles.text}>
+    <p clclassNameass={styles.title}>
+      Security Hardening
+    </p>
+  </div>
+</div>
+
+
+</div>
+
+
+
+
+    
+
+
   )
 }
 
