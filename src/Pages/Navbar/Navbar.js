efa1890b50bc57126c92c1f125518dace073e710
saeedatiah <66546810+saeedatiah@users.noreply.github.com>
2024-05-09 11:40:07 +0300

    add animation for navbar

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -1,20 +1,14 @@
 import React from 'react'
-import { Link } from "react-router-dom"
-import './Navbar.module.css'
+import styles from  './Navbar.module.css'
 
 
 const Navbar = () => {
   return (
-    <nav>
-    <div class="nav-content">
-      <ul class="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/">Home</Link></li>
-      </ul>
+    <div className={styles.container}>
+    <div className={styles.title}>aa</div>
+    <div className={styles.title}>aa</div>
+    <div className={styles.title}>aa</div>
     </div>
-  </nav>
   )
 }
 
